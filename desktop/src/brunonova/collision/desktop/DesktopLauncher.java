d4772f141b415d6e17140e84e7a4d656fc7dd837
Bruno Nova <brunomb.nova@gmail.com>
2017-02-12 17:55:59 +0000

    Create initial project

---
+++ desktop/src/brunonova/collision/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package brunonova.collision.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import brunonova.collision.Collision;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Collision(), config);
+	}
+}
